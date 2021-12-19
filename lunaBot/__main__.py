427d5bb609635e088a4187c883f58aae0cc60a51
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-19 20:20:41 +0530

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -901,7 +901,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I Aᴍ Aʟɪᴠᴇ")(https://telegra.ph/file/ed32643aae6f13a60862d.mp4), parse_mode=ParseMode.MARKDOWN, 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}",  "[I am now Alive!](https://telegra.ph/file/ed32643aae6f13a60862d.mp4)", parse_mode=ParseMode.MARKDOWN)
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
